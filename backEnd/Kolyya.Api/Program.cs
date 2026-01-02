8a2d8106d16a33018b349e12bed0b697cf1e6548
Ousseynou <nabalma_hussein@yahoo.fr>
2026-01-02 13:49:22 -0500

    backend updates for / route and health route

--- backEnd/Kolyya.Api/Program.cs
+++ backEnd/Kolyya.Api/Program.cs
@@ -3,46 +3,42 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// =======================
+// Services
+// =======================
+
+// Controllers (API)
+builder.Services.AddControllers();
+
+// Swagger / OpenAPI (Swashbuckle)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Database (PostgreSQL)
 var connectionString = Environment.GetEnvironmentVariable("POSTGRES_CONN");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 
+// =======================
+// App
+// =======================
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Swagger uniquement en dev
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
+
+app.MapGet("/", () => "Bienvenue sur l’API Kolyya !");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
