39b23309f24d5496c4c2f76c43caa17493caffc5
Ousseynou <nabalma_hussein@yahoo.fr>
2026-01-02 15:54:24 -0500

    test project in place

---
+++ backEnd/Kolyya.Api.Tests/SampleItemTests.cs
@@ -0,0 +1,21 @@
+using Xunit;
+using FluentAssertions;
+using Kolyya.Api;
+using Kolyya.Api.Models; 
+
+namespace Kolyya.Api.Tests
+{
+    public class SampleItemTests
+    {
+        [Fact]
+        public void SampleItem_Should_Set_Name_Correctly()
+        {
+            var item = new SampleItem
+            {
+                Name = "Test"
+            };
+
+            item.Name.Should().Be("Test");
+        }
+    }
+}
